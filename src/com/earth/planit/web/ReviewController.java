a9b0862f7f8aa6c93d8c09dd6624a349983571da
allsunnyday <jeonghyunsun23@gmail.com>
2018-10-01 01:30:43 +0900

    select book page 모달창
    모달 및 제이쿼리 완성

--- src/com/earth/planit/web/ReviewController.java
+++ src/com/earth/planit/web/ReviewController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ReviewController {
 
 	@RequestMapping("/review/myreview/Write.it")
-	public String reviewForm()throws Exception{
+	public String reviewDetail()throws Exception{
+		//review정보를 가지고 오기 
+		// 리뷰 번호를 가지고 오기 (이때, 리뷰는 플래너의 정보를 함께 가지고 있는 것이 좋을 듯하다 
 		return "review/myreview/WriteReview.theme";
 	}
 	
@@ -18,6 +20,9 @@ public class ReviewController {
 	
 	@RequestMapping("/planit/review/reviewView.it")
 	public String reviewView()throws Exception{
+		// 하나의 리뷰를 화면에 보여준다. 
+		// db에서 리뷰 1개의 정보 등을 가지고 온다. 
+		
 		return "tourinfo/reviewpick/ReviewView.theme";
 	}
 	
@@ -29,6 +34,10 @@ public class ReviewController {
 	//////////////////////////////////포토북
 	@RequestMapping("/photobook/step1/selectdesign.it")
 	public String selectbook()throws Exception{
+		//  사용자가 작성한 리뷰의 정보를 그대로 가져간다. 
+		// 포토북 리스트를 가지고 온다. 
+		
+		
 		return "review/photobook/SelectBook.theme";
 	}
 	
